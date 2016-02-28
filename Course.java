f13a4157fae792f9cca376ac4921331dcd5af29d
ErikSearle <esearle@unbc.ca>
2016-02-28 00:13:30 -0800

    Update Course.java to be compatible with MasterSchedule.java
    Had to change the arguments for the Course constructor, which, in turn forced a couple of other small changes.

--- Course.java
+++ Course.java
@@ -1,13 +1,12 @@
 
-import java.util.List;
-import java.util.LinkedList;
+import java.util.*;
 /**
  * 
  */
 
 /**
- * @author Sam Kelly
- * Version Feb 21/2016 1.0
+ * @author Sam Kelly (Updated by Erik Searle)
+ * Version Feb 28/2016 1.0
  */
 public class Course {
 	//defines new and unique course names for each individual course object
@@ -27,14 +26,14 @@ public class Course {
 	
 	//Course object constructor, takes in an array of Strings containing
 	//all the lines of data for a single course (used to define/create courseComponents, etc)
-	public Course(String[] sections){
-		setName(sections[0]);
+	public Course(ArrayList<String[]> sections){
+		setName(sections.get(0)[0]);
 		
 		//creates a course component for every line in the course sections data,
 		//and adds the course component object to the linked list of 
 		//components for the course
-		for (int i = 0; i < sections.length; i++) {
-			courseSections.add(CourseComponent(sections[i]));
+		for (int i = 0; i < sections.size(); i++) {
+			courseSections.add(new CourseComponent(sections.get(i)));
 			components += 1;
 		}
 	}
@@ -45,11 +44,7 @@ public class Course {
 	//the array of lines sent to the course to find the name
 	//of the course that the lines belong to
 	public void setName(String line){
-		String name = "";
-		for(int i = 0; i < 7; i++){
-			name += line.charAt(i);
-		}
-		this.courseName = name;
+		String name = line;
 	}
 	
 	public String getName(){
