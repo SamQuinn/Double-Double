7bd2b34b23e2b84b91d8ffe3f7f2dce540d70b6d
Sam Quinn Kelly <sam.q.kelly@gmail.com>
2016-03-04 14:51:39 -0800

    Update CourseComponent.java
    Wrote a version of the CourseComponent class

--- CourseComponent.java
+++ CourseComponent.java
@@ -1,24 +1,47 @@
+
+import java.util.*;
 /**
-*
-*CourseComponent class for Final Project
-*
-*@author Scott Howes
-*@version 
-*
-*/
-
-
-
-public class CourseComponent
-{
-
-	//class variable(s)
-
-
-	//Constructor
-	public CourseComponent()
-	{
-		//stuff
+ * @author Sam Kelly
+ *March 4/2016 V1.0
+ */
+public class CourseComponent {
+	private String componentName;
+	private ArrayList<Meeting> allMeetings;
+	public static int meetings = 0;
+	
+	
+	public void CourseComponent(ArrayList<String[]> section){
+		this.componentName = section.get(0)[1];
+		
+		for(int i = 0; i < section.size(); i ++){
+			this.allMeetings.add(new Meeting(section.get.(i)));
+			meetings += 1;
+		}
 	}
 	
+	public void setName(String name){
+		this.componentName = name;
+	}
+	public String getName(){
+		return this.componentName;
+	}
+	
+	public ArrayList<Meeting> getMeetings(){
+		return this.allMeetings;
+	}
+	
+	public void addMeeting(Meeting newM){
+		this.allMeetings.add(newM);
+		meetings += 1;
+	}
+	
+	public void removeMeeting(Meeting oldM){
+		if(this.allMeetings.contains(oldM)){
+			this.allMeetings.remove(oldM);
+			meetings -= 1;
+		} else {
+			System.out.println("This course component does not contain the meeting specified.");
+		}
+		
+	}
 }
