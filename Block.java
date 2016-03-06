f7f450639481721920e322501caa79c7aae37a91
NonGravity <raysongrealm@gmail.com>
2016-03-06 13:01:03 -0800

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,1 +1,20 @@
 
+public class Block {
+	
+	private String startTime;
+	private String day;
+	
+	public Block(String st,String d){
+		startTime.equals(st);
+		day.equals(d);		
+	}
+	public String getStartTime(){
+		return startTime;
+	}
+	public String getDayOfWeek(){
+		return day;
+	}
+	
+}
+
+
