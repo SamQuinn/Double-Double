97bbb56cac5fae377ef1243c361152f08405c426
ErikSearle <esearle@unbc.ca>
2016-03-14 01:39:21 -0700

    Update MasterSchedule.java

--- MasterSchedule.java
+++ MasterSchedule.java
@@ -1,45 +1,51 @@
+
 /**
 *@Author ErikSearle
 *@Version February-27-2016
 */
-
 import java.io.*;
 import java.util.*;
 import java.lang.*;
-
 public class MasterSchedule{
 	File myCSV;
 	ArrayList<Course> myCourses;
 	ArrayList<String[]> myLines;
 	Scanner scan;
-
-
-	//there are two constructors for MasterSchedule which work identically and just depend 
+	
+	
+	//there are two constructors for MasterSchedule which work identically and just depend
 	//on whether the .csv file is passed or just the filepath
 	public MasterSchedule(String a){
 		myCSV = new File(a);
 		try{
-		scan = new Scanner(myCSV);
+			scan = new Scanner(myCSV);
 		}catch(FileNotFoundException e){
 			System.out.println("File Not Found");
 		}
 		myLines = organizeCSV();
 		myCourses = courseCreator();
+		for(int i=0; i<myCourses.size(); i++){
+			System.out.println(myCourses.get(i).getCourseID());
+		}
 	}
-
-
+	
+	
 	public MasterSchedule(File a){
 		myCSV = a;
 		try{
-		scan = new Scanner(myCSV);
+			scan = new Scanner(myCSV);
 		}catch(FileNotFoundException e){
 			System.out.println("File Not Found");
 		}
 		myLines = organizeCSV();
 		myCourses = courseCreator();
+//		for(int i=0; i<myCourses.size(); i++){
+//			System.out.println(myCourses.get(i).getCourseID());
+//		}
 	}
-
-	//This method simply breaks the .csv into its lines and splits those lines at commas
+	
+	
+//	This method simply breaks the .csv into its lines and splits those lines at commas
 	private ArrayList<String[]> organizeCSV(){
 		ArrayList<String[]> lines = new ArrayList<String[]>();
 		while(scan.hasNextLine()){
@@ -47,26 +53,27 @@ public class MasterSchedule{
 		}
 		return lines;
 	}
-
-	//This method creates the arraylist of course objects by grouping all the lines with the same 
-	//course name into an ArrayList<String[]> and passing that to the Course constructor
+	
+	
+//	This method creates the arraylist of course objects by grouping all the lines with the same
+//	course name into an ArrayList<String[]> and passing that to the Course constructor
 	private ArrayList<Course> courseCreator(){
 		ArrayList<Meeting> oneCourse = new ArrayList<Meeting>();
 		ArrayList<Course> courses = new ArrayList<Course>();
-		String currentName = "placeholder";
-		int currentNumber = 0;
-
-		//The first iteration of this loop will simply set the initial values for currentName and 
-		//currentNumber. After the first iteration, everytime the loop reaches a new course it
-		//will bundle all of the components of the previous course and use them to create a course
-		//object. Then it will set the currentName and currentNumber variables to the new course
-		//and begin bundling again.
-		for(int i=0; i<myLines.size(); i++){
+		String currentName = myLines.get(4)[0];
+		int currentNumber = 4;
+		
+		
+//	The first iteration of this loop will simply set the initial values for currentName and
+//	currentNumber. After the first iteration, every time the loop reaches a new course it
+//	will bundle all of the components of the previous course and use them to create a course
+//	object. Then it will set the currentName and currentNumber variables to the new course
+//	and begin bundling again.
+		for(int i=4; i<myLines.size(); i++){
 			if(!myLines.get(i)[0].equals(currentName)){
 				for(int j=currentNumber; j<i; j++){
-					oneCourse.add(new Meeting(myLines.get(j));
+					oneCourse.add(new Meeting(myLines.get(j)));
 				}
-				
 				courses.add(new Course(oneCourse));
 				oneCourse.clear();
 				currentName = myLines.get(i)[0];
@@ -74,5 +81,17 @@ public class MasterSchedule{
 			}
 		}
 		return courses;
+	}
+	
+	public String[] getAllCourseNames(){
+		String[] names = new String[myCourses.size()];
+		for(int i=0; i<myCourses.size(); i++){
+			names[i] = myCourses.get(i).toString();
+		}
+		return names;
+	}
+	
+	public Object[] getCourses(){
+		return myCourses.toArray();
 	}
 }
