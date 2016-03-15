00e6b406c289668eb3e3e7e246755c9fc296b519
ErikSearle <esearle@unbc.ca>
2016-03-15 11:50:47 -0700

    Update MasterSchedule.java

--- MasterSchedule.java
+++ MasterSchedule.java
@@ -11,6 +11,9 @@ public class MasterSchedule{
 	ArrayList<Course> myCourses;
 	ArrayList<String[]> myLines;
 	Scanner scan;
+	ArrayList<Prof> myProfs;
+	ArrayList<Room> myRooms;
+	ArrayList<Meeting> myMeetings;
 	
 	
 	//there are two constructors for MasterSchedule which work identically and just depend
@@ -23,10 +26,10 @@ public class MasterSchedule{
 			System.out.println("File Not Found");
 		}
 		myLines = organizeCSV();
+		myMeetings = new ArrayList<Meeting>();
 		myCourses = courseCreator();
-		for(int i=0; i<myCourses.size(); i++){
-			System.out.println(myCourses.get(i).getCourseID());
-		}
+		myProfs = profCreator();
+		myRooms = roomCreator();
 	}
 	
 	
@@ -38,10 +41,12 @@ public class MasterSchedule{
 			System.out.println("File Not Found");
 		}
 		myLines = organizeCSV();
+		myMeetings = new ArrayList<Meeting>();
 		myCourses = courseCreator();
-//		for(int i=0; i<myCourses.size(); i++){
-//			System.out.println(myCourses.get(i).getCourseID());
-//		}
+		System.out.println(myLines.size() + "" + myMeetings.size());
+		myProfs = profCreator();
+		myRooms = roomCreator();
+
 	}
 	
 	
@@ -73,6 +78,7 @@ public class MasterSchedule{
 			if(!myLines.get(i)[0].equals(currentName)){
 				for(int j=currentNumber; j<i; j++){
 					oneCourse.add(new Meeting(myLines.get(j)));
+					myMeetings.add(new Meeting(myLines.get(j)));
 				}
 				courses.add(new Course(oneCourse));
 				oneCourse.clear();
@@ -82,7 +88,67 @@ public class MasterSchedule{
 		}
 		return courses;
 	}
-	
+
+	private ArrayList<Prof> profCreator(){
+		ArrayList<Prof> profs = new ArrayList<Prof>();
+		ArrayList<Meeting> oneProf = new ArrayList<Meeting>();
+		String currentProf = "";
+
+		for(int i=0; i<myMeetings.size(); i++){
+			if(!myMeetings.get(i).getProfessor().equals("")){
+				if(!Character.isUpperCase(myMeetings.get(i).getProfessor().charAt(1))){
+					if(profs.size()==0){
+						currentProf = myMeetings.get(i).getProfessor();
+						for(int j=i; j<myMeetings.size(); j++){
+							if(myMeetings.get(j).getProfessor().equals(currentProf)) oneProf.add(myMeetings.get(j));
+						}
+						profs.add(new Prof(oneProf, currentProf));
+					}
+					for(int h=0; h<profs.size(); h++){
+						if(myMeetings.get(i).getProfessor().equals(profs.get(h).getName())) break;
+						else if(h==profs.size()-1){
+							currentProf = myMeetings.get(i).getProfessor();
+							for(int j=i; j<myMeetings.size(); j++){
+								if(myMeetings.get(j).getProfessor().equals(currentProf)) oneProf.add(myMeetings.get(j));
+							}
+							profs.add(new Prof(oneProf, currentProf));
+						}							
+					}
+				}
+			}
+		}
+		return profs;
+	}
+
+	private ArrayList<Room> roomCreator(){
+		ArrayList<Room> rooms = new ArrayList<Room>();
+		ArrayList<Meeting> oneRoom = new ArrayList<Meeting>();
+		String currentRoom = "";
+
+		for(int i=0; i<myMeetings.size(); i++){
+			if(!myMeetings.get(i).getRoomNumber().equals("")){
+				if(rooms.size()==0){
+						currentRoom = myMeetings.get(i).getRoomNumber();
+						for(int j=i; j<myMeetings.size(); j++){
+							if(myMeetings.get(j).getRoomNumber().equals(currentRoom)) oneRoom.add(myMeetings.get(j));
+						}
+						rooms.add(new Room(oneRoom, currentRoom));
+				}
+				for(int h=0; h<rooms.size(); h++){
+					if(myMeetings.get(i).getRoomNumber().equals(rooms.get(h).getRoomNumber())) break;
+					else if(h==rooms.size()-1){
+						currentRoom = myMeetings.get(i).getRoomNumber();
+						for(int j=i; j<myMeetings.size(); j++){
+							if(myMeetings.get(j).getRoomNumber().equals(currentRoom)) oneRoom.add(myMeetings.get(j));
+						}
+						rooms.add(new Room(oneRoom, currentRoom));
+					}							
+				}
+			}
+		}
+		return rooms;
+	}
+
 	public String[] getAllCourseNames(){
 		String[] names = new String[myCourses.size()];
 		for(int i=0; i<myCourses.size(); i++){
@@ -93,5 +159,13 @@ public class MasterSchedule{
 	
 	public Object[] getCourses(){
 		return myCourses.toArray();
+	}
+
+	public Object[] getProfs(){
+		return myProfs.toArray();
+	}
+
+	public Object[] getRooms(){
+		return myRooms.toArray();
 	}
 }
